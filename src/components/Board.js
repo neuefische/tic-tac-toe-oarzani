5eb5d0a0e2dc30394265c7ba9f6f4c413349c4f9
Omid Arzani <omid.arzani@gmx.net>
2019-09-10 11:49:45 +0200

    add components

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,27 +1,32 @@
 import React from "react";
 import Square from "./Square";
 
-export default function Board() {
+function renderSquare(index) {
+  return <Square value={index} />;
+}
+
+function Board() {
   const status = "Next player: X";
 
   return (
     <div>
       <div className="status">{status}</div>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        {renderSquare("dassssss")}
+        {renderSquare(0)}
+        {renderSquare(3)}
       </div>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        {renderSquare(0)}
+        {renderSquare("b")}
+        {renderSquare(0)}
       </div>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        {renderSquare(0)}
+        {renderSquare(0)}
+        {renderSquare(0)}
       </div>
     </div>
   );
 }
+export default Board;

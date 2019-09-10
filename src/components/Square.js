051a5761e759195823979b19e1fc598110ddf718
Omid Arzani <omid.arzani@gmx.net>
2019-09-10 14:58:12 +0200

    Properties usw

--- src/components/Square.js
+++ src/components/Square.js
@@ -1,5 +1,21 @@
 import React from "react";
 
+// export default function Square(props) {
+//   const [state, setState] = React.useState({ value: null });
+//   return (
+//     <button className="square" onClick={() => alert("click")}>
+//       {props.value}
+//     </button>
+//   );
+// }
+// das onClick ersetzt den addEventListener
+
+// ersetze man das ("click") durch (props.value) würden die jeweiligen Inhalte aus Kästchen im ALERT erscheinen.
 export default function Square(props) {
-  return <button className="square">{props.value}</button>;
+  const [state, setState] = React.useState({ value: null });
+  return (
+    <button className="square" onClick={() => props.onClick()}>
+      {props.value}
+    </button>
+  );
 }

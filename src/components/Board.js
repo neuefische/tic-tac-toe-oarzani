bf602be0189f046466983e9c6f980a6acc0e06b5
Omid Arzani <omid.arzani@gmx.net>
2019-09-10 15:58:33 +0200

    add NextPlayer

--- src/components/Board.js
+++ src/components/Board.js
@@ -10,26 +10,37 @@ function Board() {
     squares: Array(9).fill(null),
     xIsNext: true
   });
-
   // function handleClick(index) {
   //   alert(index);
   // }
-
   function handleClick(index) {
     // Immutability
     const squaresCopy = state.squares.slice();
     squaresCopy[index] = state.xIsNext ? "X" : "0";
     setState({ squares: squaresCopy, xIsNext: !state.xIsNext });
-
     // mittels sclice wird ein Array kopiert anstatt es zu bennenen.
   }
-
   function renderSquare(index) {
     return (
       <Square value={state.squares[index]} onClick={() => handleClick(index)} />
     );
   }
-  const status = "Next player: X";
+  // state.xIsNext
+
+  // const status = state.xIsNext ? "Next Player: X" : "Next Player: O";
+
+  //Optimale Lösung :
+  const status = `Next player: ${state.xIsNext ? "X" : "O"}`;
+
+  //Lenas Ansatz
+  // function getNextPlayer(xIsNext) {
+  //   if (state.xIsNext) {
+  //     return "Next player: X";
+  //   } else {
+  //     return "Next player: 0";
+  //   }
+  // }
+  // const status = getNextPlayer(state.xIsNext);
 
   // in der KOnsole : Array(9).fill(null) zeigt dann neun mal im null im Array an. squares in dem Fall eine Propertie.
 
